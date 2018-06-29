813a74f30fed43c8241021dd8b433aee49845b21
lycHub <1120805364@qq.com>
2018-06-29 20:04:52 +0800

    结束

--- build/webpack.prod.js
+++ build/webpack.prod.js
@@ -7,3 +7,11 @@ module.exports = {
     runtimeChunk: true
   }
 }
+
+
+// optimization: {
+//   splitChunks: {
+//     name: 'common',     // 打包公共代码的文件名
+//       minChunks: 2        // 出现两次以上就打包
+//   }
+// }
